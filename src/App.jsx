7e2f57d3000ef44ac9215d9aa8fc51633d044720
danAnasuuuuu <anasishaq7110@gmail.com>
2025-12-28 16:46:34 +0100

    created interface for Tenzee project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import Die from "./components/Die"
+import { useState } from "react"
+import Die from "./Die"
+import { nanoid } from "nanoid"
 
 export default function App() {
+    /**
+     * Challenge: Add conditional styling to the Die component
+     * so that if it's held (isHeld === true), its background color
+     * changes to a light green (#59E391)
+     * 
+     * Remember: currently the Die component has no way of knowing
+     * if it's "held" or not.
+     */
+    
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
+    }
+
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+
+    const diceElements = dice.map(dieObj => (
+        <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld}/>)
+    )
+
     return (
         <main>
             <div className="dice-container">
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
-                <Die value={1} />
+                {diceElements}
             </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
