07043c16c73217f26113fbd2177f259807c18ec8
danAnasuuuuu <anasishaq7110@gmail.com>
2025-12-29 20:09:12 +0100

    update to Tenzee game(ie. the Capstone project), added some accesibility feature and a focus on the new game button when the game is won

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
 import Confetti from "react-confetti"
 
 export default function App() {
     const [dice, setDice] = useState(() => generateAllNewDice())
+    const buttonRef = useRef(null)
 
     const gameWon = dice.every(die => die.isHeld) &&
         dice.every(die => die.value === dice[0].value)
+        
+    useEffect(() => {
+        if (gameWon) {
+            buttonRef.current.focus()
+        }
+    }, [gameWon])
 
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
             .map(() => ({
-                value: Math.ceil(Math.random() * 6),
+                // value: Math.ceil(Math.random() * 6),
+                value: 5,
                 isHeld: false,
                 id: nanoid()
             }))
@@ -51,14 +59,17 @@ export default function App() {
     return (
         <main>
             {gameWon && <Confetti />}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>
+            <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
                 {gameWon ? "New Game" : "Roll"}
             </button>
         </main>
     )
-} 
+}
