e7aa7210a77f892e3df87d04c0e15d9a768b27ca
danAnasuuuuu <anasishaq7110@gmail.com>
2026-01-15 06:28:40 +0100

    base interface implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,60 @@
 import React from "react"
+import { languages } from "./languages"
 
-/**
- * Goal: Build out the main parts of our app
- * 
- * Challenge: Add a header with the game title
- * and description. Startin' out easy 🙂🚶‍➡️
- */
+export default function AssemblyEndgame() {
+  
+  // programming languages list 
+  const langCard = languages.map((lang) =>{
+    const style = {
+      backgroundColor: lang.backgroundColor, 
+      color: lang.color
+    }
+  
+    return (    
+      <span className = "language" key={lang.name} style={style}>
+      {lang.name}
+      </span>
+      )
+  })
 
-export default function Hangman() {
+  
+  // selected word place holder and state variable
+  const [word, setWord] = React.useState("python")
+  const letterElements = word.toUpperCase().split("").map((char, index) => (
+    <span key={index}>{char}</span>
+  ))
+
+
+  // keyboard keys
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const keyBoardButtons = alphabet.toUpperCase().split("").map((char, index) => (
+    <button key={index}>{char}</button>
+  ))
+
+
     return (
         <main>
-            Game goes here
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the 
+                programming world safe from Assembly!</p>
+            </header>
+            <main>
+              <section className="message">
+                <h2>You Win</h2>
+                <p>Well done! 🎉</p>
+              </section>
+              <section className="languages">
+                {langCard}
+              </section>
+              <section className="selected-word">
+                {letterElements}
+              </section>
+              <section className="keyboard">
+                {keyBoardButtons}
+              </section>
+              <button className="new-game">New Game</button>
+            </main>
         </main>
     )
 }
---
+++ src/Language.jsx
@@ -0,0 +1,15 @@
+// export default function Language(props){
+//     const style = {
+//         backgroundColor: props.back, 
+//         color: props.color, 
+//         display: "inline", 
+//         padding: "4px", 
+//         borderRadius: "4px",
+//         margin: "2px 5px"
+//     }
+//     return (
+//         <div> style={style}
+//         props.name
+//         </div>
+//     )
+// }
