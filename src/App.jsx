bb20617aa158aa6d8b9acdb3b7f0129cbec0ae42
danAnasuuuuu <anasishaq7110@gmail.com>
2026-02-01 16:51:57 +0100

    basic funtionalities of the game including win, lose state and reset was implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
 import React from "react"
 import { languages } from "./languages"
+import clsx from 'clsx';
+import {getFarewellText} from "./utils"
 
 export default function AssemblyEndgame() {
+
+  // state values
+  const [guessedLetters, setGuessedLetters] = React.useState([])
+  const [word, setWord] = React.useState("assembly")
+
+
+  // static values
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  let fareWellTextMessage = ""  
+
+
+  // derived values
+  const wrongGuessCount = guessedLetters.filter(letter => !word.toUpperCase().split("").includes(letter)).length
+  // console.log(guessedLetters)
+  console.log("wrong guess coutnt: ", wrongGuessCount)
+  const isGameWon = word.toUpperCase().split("").every(letter => guessedLetters.includes(letter))
+  console.log("is game won: ", isGameWon)
+  const isGameLost = wrongGuessCount >= languages.length -1
+  const isGameOver = isGameWon || isGameLost
+  console.log("is game over: ", isGameOver)
+  
   
   // programming languages list 
-  const langCard = languages.map((lang) =>{
+  const langCard = languages.map((lang, index) =>{
     const style = {
       backgroundColor: lang.backgroundColor, 
       color: lang.color
     }
+
+ 
   
     return (    
-      <span className = "language" key={lang.name} style={style}>
+      <span className = {clsx({
+        language: true,
+        lost : wrongGuessCount > index,
+
+      })} 
+      key={index} 
+      style={style}>
       {lang.name}
       </span>
       )
@@ -19,18 +50,68 @@ export default function AssemblyEndgame() {
 
   
   // selected word place holder and state variable
-  const [word, setWord] = React.useState("python")
-  const letterElements = word.toUpperCase().split("").map((char, index) => (
-    <span key={index}>{char}</span>
-  ))
+  const letterElements = word.toUpperCase().split("").map((char, index) => {
+    const isGuessed = guessedLetters.includes(char)
+    if(isGuessed){
+      return(
+        <span key={index}>{char}</span>
+      )
+    }else{
+      return(
+        <span key={index}></span>
+      )
+    }
+  })
 
+  
+  // console.log(guessedLetters)
 
   // keyboard keys
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-  const keyBoardButtons = alphabet.toUpperCase().split("").map((char, index) => (
-    <button key={index}>{char}</button>
-  ))
+    const keyBoardButtons = alphabet.toUpperCase().split("").map((char, index) => {
+    const isGuessed = guessedLetters.includes(char)
 
+    const isCorrect = isGuessed && word.toUpperCase().split("").includes(char)
+    const isWrong = isGuessed && !word.toUpperCase().split("").includes(char)
+
+    
+
+    return(
+    <button 
+    onClick={handleKeyboardClick}
+    className={clsx(isCorrect && "correct", isWrong && "wrong")}
+    key={index}>{char}</button>
+    )
+})
+
+
+
+  function handleKeyboardClick(e) {
+    // collect the button text from event object e
+    const letter = e.target.innerText
+    // add previous values to a set in order to avoid duplicate, then convert it to array
+    setGuessedLetters(prevGuessedLetters => {
+      const lettersSet = new Set(prevGuessedLetters)
+      lettersSet.add(letter)
+      if (!word.includes(letter)){
+        fareWellTextMessage = getFarewellText(languages[wrongGuessCount].name)
+        console.log(fareWellTextMessage)
+      }
+      return Array.from(lettersSet)
+    })
+    console.log(guessedLetters)
+  }
+ 
+  function reset() {
+    setGuessedLetters([])
+    setWord("assembly")
+  }
+
+
+  const className = clsx({
+    message: true,
+    won: isGameWon,
+    lost: isGameLost
+    })
 
     return (
         <main>
@@ -40,10 +121,29 @@ export default function AssemblyEndgame() {
                 programming world safe from Assembly!</p>
             </header>
             <main>
-              <section className="message">
-                <h2>You Win</h2>
-                <p>Well done! 🎉</p>
-              </section>
+              
+              
+                <section className={className}>
+                  {
+                    isGameWon? (
+                      <>
+                      <h2>You Win</h2>
+                      <p>Well done! 🎉</p>
+                      </>
+                    ) : (
+                      isGameLost? (
+                        <>
+                        <h2>Game over!</h2>
+                        <p>You lose! Better start learning Assembly 😭</p>
+                        </>
+                      ) : (
+                        ""
+                      )
+                    )
+                  }
+                </section>
+              
+             
               <section className="languages">
                 {langCard}
               </section>
@@ -53,7 +153,8 @@ export default function AssemblyEndgame() {
               <section className="keyboard">
                 {keyBoardButtons}
               </section>
-              <button className="new-game">New Game</button>
+              
+              {isGameOver  && (<button className="new-game" onClick={reset}>New Game</button>)}
             </main>
         </main>
     )
